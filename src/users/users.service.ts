45734014ca33ae252893102c7a129ad8806e4123
rakib3719 <bannah76769@gmail.com>
2026-05-25 17:21:38 +0600

    refactor: reorder import statements in UsersService for consistency

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service.js';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Prisma } from '../generated/prisma/client.js';
+import { PrismaService } from '../prisma/prisma.service.js';
 
 @Injectable()
 export class UsersService {
